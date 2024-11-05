5f6b3fd7cbe236b9e40ba8ed69a17c13aa88adef
01Master00 <berkibalintka@gmail.com>
2024-11-05 10:11:55 +0100

    added a prime number list creator

--- listodus/Program.cs
+++ listodus/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (var item in PrimSzamok(2, 10))
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
+        static List<int> PrimSzamok(int kezdo, int veg)
+        {
+            List<int> prim = new List<int>();
+            for (int i = kezdo; i < veg; i++)
+            {
+                if (Prima(i)) 
+                    prim.Add(i);
+            }
+            return prim;
+        }
+
+        static bool Prima(int szam)
+        {
+            if (szam < 2)
+                return false;
+            for (int i = 2; i < szam; i++)
+            {
+                if (szam%i == 0)
+                    return false;
+            } 
+            return true;
+        }   
+
     }
+
 }
